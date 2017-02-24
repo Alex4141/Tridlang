d5d151bc09883792396b5000ca60a40245fead90
Alex4141 <agoncalves4141@aol.com>
2017-02-23 22:15:40 -0500

    Worked on parser functions, working towards 'if' parsing

--- parser/parser.js
+++ parser/parser.js
@@ -8,6 +8,49 @@ function Parser(tokenStream){
 
 	var pos = -1;
 	var currentToken = this.getToken();
+	
+	// Main Functions to parse the Tokens
+	
+	// Utility Functions to handle Parser
+	
+	Parser.prototype.parseIfStatment = function(){
+		this.skipKeyWord("if");
+		this.skipOperator('(');
+		var exp = parseExpression();
+		// TODO Finish Parse Expression 
+	};
+
+	Parser.prototype.parseExpression = function(){
+		// Write individual parse function
+		return 
+	};
+
+	Parser.prototype.isKeyword = function(value){
+		return current.type == "Keyword" && current.value == value;
+	};
+
+	Parser.prototype.skipKeyword = function(value){
+		if(this.isKeyword(value)){ this.advanceToken(); }
+		else { throw "Syntax Error"; }
+	};
+
+	Parser.prototype.isPunctuation = function(value){
+		return current.type == "Punctuation" && current.value == value;
+	};
+
+	Parser.prototype.skipPunctuation = function(value){
+		if(this.isPunctuation(value)) { this.advanceToken(); }
+		else { throw "Syntax Error"; }
+	};
+
+	Parser.prototype.isOperator = function(value){
+		return current.type == "Operator" && current.value == value;
+	};
+
+	Parser.prototype.skipOperator = function(value){
+		if(this.isOperator(value)) { this.advanceToken(); }
+		else { throw "Syntax Error" }
+	};
 
 	// Utility Functions to handle Token Stream
 
